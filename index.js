6e1cf58749cbcfd8191f10dd5767bea7d68da38e
Tamarana Haimavathi <hymatamarana2000@gmail.com>
2024-04-10 09:09:32 +0000

    Congratscardapp

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+    <div className="congrats">
+        <div className="container">
+            <h1 className="heading">Congratulations</h1>
+            <div className="card">
+                <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"</img>
+                <h1 className="heading">Kiran V</h1>
+                <p className="paragraph">Vishnu Institute of Computer Education and Technology,Bhimavaram</p>
+                <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"</img>
+
+            </div>
+        </div>
+    </div>
+  );
+  
+ReactDOM.render(element, document.getElementById('root'));
